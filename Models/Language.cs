efc01fc1025af2c587d95ab09b50b212ee37ce4f
Catalin <catalin.cosma@softwareone.com>
2022-02-11 11:35:28 +0100

    Add the user language model

--- Models/Language.cs
+++ Models/Language.cs
@@ -3,6 +3,7 @@
     public class Language
     {
         public long Id { get; set; }
+
         public string Name { get; set; }
 
         public Language(long id, string name)
---
+++ Models/UserLanguage.cs
@@ -0,0 +1,18 @@
+﻿namespace SkillMatrix.Models
+{
+    public class UserLanguage
+    {
+        public long Id { get; set; }
+
+        public Language language;
+
+        public int Level;
+
+        public UserLanguage(long id, Language language, int level)
+        {
+            Id = id;
+            this.language = language;
+            Level = level;
+        }
+    }
+}
