7b8599cbf90ac5414851020ec49e32052d82e1ab
Catalin <catalin.cosma@softwareone.com>
2022-02-11 11:32:12 +0100

    Add the skill model

---
+++ Models/Skill.cs
@@ -0,0 +1,18 @@
+﻿namespace SkillMatrix.Models
+{
+    public class Skill
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public SkillCategory skillCategory;
+
+        public Skill(long id, string name, SkillCategory skillCategory)
+        {
+            Id = id;
+            Name = name;
+            this.skillCategory = skillCategory;
+        }
+    }
+}
