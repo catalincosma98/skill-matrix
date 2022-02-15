d5f31a5b8f04cb5955c556fd8f0f087cc1eb27a6
Catalin <catalin.cosma@softwareone.com>
2022-02-15 09:32:35 +0100

    Add generic repository and interface for the repository pattern

---
+++ Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace SkillMatrix.Interfaces
+{
+    public interface IGenericRepository<T>
+    {
+        List<T> FindAll();
+        List<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using SkillMatrix.Database;
+using SkillMatrix.Interfaces;
+
+namespace SkillMatrix.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly DataContext DataContext;
+
+        public GenericRepository(DataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+        public List<T> FindAll()
+        {
+            return DataContext.Set<T>().AsNoTracking().ToList();
+        }
+        public List<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return DataContext.Set<T>().Where(expression).AsNoTracking().ToList();
+        }
+        public void Create(T entity)
+        {
+            DataContext.Set<T>().Add(entity);
+        }
+        public void Update(T entity)
+        {
+            DataContext.Set<T>().Update(entity);
+        }
+        public void Delete(T entity)
+        {
+            DataContext.Set<T>().Remove(entity);
+        }
+    }
+}
